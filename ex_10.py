a342bb5a4d395cf1355fd8d30c3f45724ee001af
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-07-02 21:12:32 +0300

    Update ex_10.py

--- ex_10.py
+++ ex_10.py
@@ -1,11 +1,9 @@
-formula = input("Enter calculation of the numerical digits, format: X + Y = Z ")
+formula = input("PLease enter calculation, format: X + Y = Z ")
 
-lst = formula.split(" + ")
+lst = formula.split()
 x = int(lst[0])
-del lst[0]
-lst = str(lst[0]).split(" = ")
-y = int(lst[0])
-z = int(lst[1])
+y = int(lst[2])
+z = int(lst[4])
 
 if x + y == z:
     print(True)
